842cc7a3d16a584ce9bed4bd120acbeb03e4c1e5
Laura You-Li Chen <lauraylchen@hotmail.com>
2022-02-14 15:51:47 -0500

    Completed action #score for controller games

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,24 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
     @letters = 10.times.map { ('A'..'Z').to_a[rand(26)] }
   end
 
-  def score; end
+  def score
+    url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    json_string = URI.open(url).read
+    json_hash = JSON.parse(json_string)
+    word = params[:word]
+    letters = params[:letters].split(' ')
+
+    @message = if json_hash['found'] == false
+                 "Sorry but #{word.upcase} does not seem to be a valid English word ..."
+               elsif word.chars.all? { |letter| word.count(letter) <= letters.count(letter.upcase) } == false
+                 "Sorry but #{word.upcase} can't be built out of #{letters.join(',')}"
+               else
+                 "Congratulations! #{word.upcase} is a valid English word!"
+               end
+  end
 end
